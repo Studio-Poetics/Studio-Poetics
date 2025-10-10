78ba7aa6db047d3f46b2b221b3d288442142e160
Pranshu Chaudhary <wittywit@users.noreply.github.com>
2025-10-10 17:06:10 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Studio-Poetics' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/Studio-Poetics' : '',
 }
 
 export default nextConfig
